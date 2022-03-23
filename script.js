7f34b8764e538af6df3efed0747e974b5c311991
Otávio Azevedo <otavioad1997@gmail.com>
2022-03-23 11:50:48 -0300

    Alteração

--- script.js
+++ script.js
@@ -8,16 +8,17 @@ function addLista(event) {
   event.preventDefault();
 
   const tarefas = document.createElement('li');
-  tarefas.classList.add("lista");
+  tarefas.classList.add('lista');
   listaTarefas.appendChild(tarefas);
   tarefas.innerText = textoTarefa.value;
 
   if (textoTarefa.value.length !== 0) {
     textoTarefa.value = null;
-    console.log(textoTarefa.value);
-
-    tarefas.addEventListener('click', function corCinza(event) {
-        event.target.style.backgroundColor = 'gray';
-    });
+  } else if (textoTarefa.value.length === 0) {
+    alert('Vazio!');
+    listaTarefas.removeChild(tarefas);
   }
+  tarefas.addEventListener('click', function corCinza(event) {
+    event.target.style.backgroundColor = 'gray';
+  });
 }
