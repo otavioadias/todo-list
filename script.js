360e4d1a0551ee67a754a8f3e8267e3fec1fbd45
Otávio Azevedo <otavioad1997@gmail.com>
2022-03-23 17:49:51 -0300

    Alteração requisito 7 e 8

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-const textoTarefa = document.getElementById('texto-tarefa');
-const btnCriarTarefa = document.getElementById('criar-tarefa');
-const listaTarefas = document.getElementById('lista-tarefas');
-
-btnCriarTarefa.addEventListener('click', addLista);
+const textoTarefa = document.querySelector('#texto-tarefa');
+const btnCriarTarefa = document.querySelector('#criar-tarefa');
+const listaTarefas = document.querySelector('#lista-tarefas');
 
 function addLista(event) {
   event.preventDefault();
 
-  const tarefas = document.createElement('li');
-  tarefas.classList.add('lista');
-  listaTarefas.appendChild(tarefas);
-  tarefas.innerText = textoTarefa.value;
+  const tarefa = document.createElement('li');
+  tarefa.classList.add('lista');
+  listaTarefas.appendChild(tarefa);
+  tarefa.innerText = textoTarefa.value;
 
   if (textoTarefa.value.length !== 0) {
     textoTarefa.value = null;
   } else if (textoTarefa.value.length === 0) {
     alert('Vazio!');
-    listaTarefas.removeChild(tarefas);
+    listaTarefas.removeChild(tarefa);
   }
-  tarefas.addEventListener('click', function corCinza(event) {
-    event.target.style.backgroundColor = 'gray';
+  addEvento(tarefa);
+}
+
+btnCriarTarefa.addEventListener('click', addLista);
+
+function removeSelected() {
+  const tarefas = document.querySelectorAll('.lista');
+  for (let index = 0; index < tarefas.length; index += 1) {
+    tarefas[index].classList.remove('selected');
+  }
+}
+
+function addEvento(li) {
+  li.addEventListener('click', function (event) {
+    if (event.target.classList[1] === 'selected') {
+      event.target.classList.remove('selected');
+      console.log(event.target.classList[0]);
+    } else {
+      removeSelected();
+      event.target.classList.add('selected');
+    }
   });
 }
