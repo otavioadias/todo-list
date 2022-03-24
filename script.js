9277645e2aa1328e83ef8ae308475885a84a81fb
Otávio Azevedo <otavioad1997@gmail.com>
2022-03-24 12:13:17 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
+
 const textoTarefa = document.querySelector('#texto-tarefa');
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
 const btnApagaTudo = document.querySelector('#apaga-tudo');
 const btnApagaCompletos = document.querySelector('#remover-finalizados'); 
-
+const btnSalvar = document.querySelector('#salvar-tarefas');
+const btnCima = document.querySelector('#mover-cima');
+const btnBaixo = document.querySelector('#mover-baixo');
 
 function addLista(event) {
   event.preventDefault();
 
-  const tarefa = document.createElement('li');
-  tarefa.classList.add('lista');
-  listaTarefas.appendChild(tarefa);
-  tarefa.innerText = textoTarefa.value;
+  const tarefas = document.createElement('li');
+  tarefas.classList.add('lista');
+  listaTarefas.appendChild(tarefas);
+  tarefas.innerText = textoTarefa.value;
 
   if (textoTarefa.value.length !== 0) {
     textoTarefa.value = null;
   } else if (textoTarefa.value.length === 0) {
     alert('Vazio!');
-    listaTarefas.removeChild(tarefa);
+    listaTarefas.removeChild(tarefas);
   }
-  addEvento(tarefa);
-  completed(tarefa);
+  addEvento(tarefas);
+  completed(tarefas);
 }
+
 
 btnCriarTarefa.addEventListener('click', addLista);
 
@@ -52,7 +56,6 @@ function completed(li) {
   li.addEventListener('dblclick', function (event) {
     if (event.target.classList[1] === 'completed') {
       event.target.classList.remove('completed');
-      console.log('clicou');
     } else  {
       event.target.classList.add('completed');      
     } 
@@ -74,3 +77,46 @@ btnApagaCompletos.addEventListener('click', function completos(event) {
     listaTarefas.removeChild(tarefaCompleta[index]);
   }
 });
+
+btnSalvar.addEventListener('click', function salvar(event) {
+  event.preventDefault();
+  const tarefas = document.querySelectorAll('.lista');
+  let arrayTarefas = [];
+  for (let index = 0; index < tarefas.length; index += 1) {
+    let objTarefa = {
+      text: tarefas[index].innerHTML,
+      class: tarefas[index].className,
+    };
+    arrayTarefas.push(objTarefa);
+  }
+  console.log(arrayTarefas);
+  localStorage.setItem('tarefas', JSON.stringify(arrayTarefas));
+ 
+});
+
+function recarregar() {
+  let localStorageTarefas = JSON.parse(localStorage.getItem('tarefas'));
+  localStorageTarefas = localStorage.getItem('tarefas') !== null ? localStorageTarefas : [];
+
+  for (let index = 0; index < localStorageTarefas.length; index +=1) {
+    const tarefa = document.createElement('li');
+    addEvento(tarefa);
+    completed(tarefa);
+    tarefa.innerHTML = localStorageTarefas[index].text;
+    tarefa.className = localStorageTarefas[index].class;
+    listaTarefas.appendChild(tarefa);
+  }
+}
+
+window.onload = function() {
+  recarregar();
+}
+
+
+/* btnCima.addEventListener('click', function moverCima(event) {
+    event.preventDefault();
+  const tarefas = document.querySelectorAll('.lista');
+  console.log('subiu');
+  for (let index = 0; index < tarefas.length; index += 1) {
+  }    
+}) */
